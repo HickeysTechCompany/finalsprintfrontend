a53bb796164a4465c4c9def0a2b558b140412737
Colonater <113359216+Colonater@users.noreply.github.com>
2023-12-14 09:15:48 -0330

    added login page to bring you to admin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import main from "./components/main";
-import { useState } from "react";
+
+
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Main from './components/main'; // Import your main component
+import Admin from './Admin';
 
 function App() {
-
   return (
-    <div className= "Links" >
-  <Router>
-  <Routes>
-  <Route path="/main" element= {<main />} />
-  </Routes>
-  </Router>
-            </div>
+    <div className="Links">
+      <Router>
+        <Routes>
+          <Route path="/main" element={<Main />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
-
 
 export default App;
