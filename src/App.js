12fc90e6d3feb6a1b59ec1664a8b540d0d8b1c6f
BrandonConnolly <119525545+BrandonConnolly@users.noreply.github.com>
2023-12-14 09:49:03 -0330

    Brandon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Main from "./components/Main";
+import main from "./components/main";
 import { useState } from "react";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className= "Links" >
   <Router>
   <Routes>
-  <Route path="/Main" element= {<Main />} />
+  <Route path="/main" element= {<main />} />
   </Routes>
   </Router>
             </div>
