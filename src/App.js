9f98cf06772206a4701e9a6b3b320248fef7f7b4
BrandonConnolly <119525545+BrandonConnolly@users.noreply.github.com>
2023-12-21 11:39:57 -0330

    Main Page done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
+ 
 import Main from "./components/Main";
 import Login from "./components/Login";
 import Credits from "./components/Credits";
-import Admin from "./components/Admin";
 import './styles.css';
-
-
-
+ 
+ 
+ 
 function App() {
   const [airport, setAirport] = useState([]);
   const [aircraft, setAircraft] = useState([]);
   const [airline, setAirline] = useState([]);
   const [gates, setGates] = useState([]);
-
   const [searchTerm, setSearchTerm] = useState('');
  
-  
-  
+ 
+ 
   useEffect(() => {
     const fetchAirport = async () => {
       try {
@@ -34,69 +32,83 @@ function App() {
         console.error("Error fetching airport data:", error.message);
       }
     };
-
+ 
     fetchAirport();
   }, []);
-  
+ 
+  useEffect(() => {
+    const fetchAircraft = async () => {
+      try {
+        const response = await fetch(`http://airportdbdocker-env.eba-r6rauie6.us-east-1.elasticbeanstalk.com/aircraft`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch Aircraft");
+        }
+        const data = await response.json();
+        console.log(data);
+        setAircraft(data._embedded?.aircraft || []);
+      } catch (error) {
+        console.error("Error fetching aircraft data:", error.message);
+      }
+    };
+ 
+    fetchAircraft();
+  }, []);
+ 
+  useEffect(() => {
+    const fetchAirline = async () => {
+      try {
+        const response = await fetch(`http://airportdbdocker-env.eba-r6rauie6.us-east-1.elasticbeanstalk.com/airline`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch Airline");
+        }
+        const data = await response.json();
+        console.log(data);
+        setAirline(data._embedded?.airline || []);
+      } catch (error) {
+        console.error("Error fetching airline data:", error.message);
+      }
+    };
+ 
+    fetchAirline();
+  }, []);
+ 
+  useEffect(() => {
+    const fetchGates = async () => {
+      try {
+        const response = await fetch(`http://airportdbdocker-env.eba-r6rauie6.us-east-1.elasticbeanstalk.com/gates`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch Gates");
+        }
+        const data = await response.json();
+        console.log(data);
+        setGates(data._embedded?.gates || []);
+      } catch (error) {
+        console.error("Error fetching gates data:", error.message);
+      }
+    };
+ 
+    fetchGates();
+  }, []);
+ 
   // useEffect(() => {
-  //   const fetchAircraft = async () => {
-  //     try {
-  //       const response = await fetch(`http://localhost:8080/aircraft`);
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch Aircraft");
-  //       }
-  //       const data = await response.json();
-  //       console.log(data);
-  //       setAircraft(data._embedded?.aircraft || []);
-  //     } catch (error) {
-  //       console.error("Error fetching aircraft data:", error.message);
-  //     }
-  //   };
-
-  //   fetchAircraft();
-  // }, []);
-  
-  // useEffect(() => {
-  //   const fetchAirline = async () => {
-  //     try {
-  //       const response = await fetch(`http://localhost:8080/airline`);
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch Airline");
-  //       }
-  //       const data = await response.json();
-  //       console.log(data);
-  //       setAirline(data._embedded?.airline || []);
-  //     } catch (error) {
-  //       console.error("Error fetching airline data:", error.message);
-  //     }
-  //   };
-
-  //   fetchAirline();
-  // }, []);
-
-  // useEffect(() => {
-  //   const fetchGates = async () => {
-  //     try {
-  //       const response = await fetch(`http://localhost:8080/gates`);
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch Gates");
-  //       }
-  //       const data = await response.json();
-  //       console.log(data);
-  //       setGates(data._embedded?.gates || []);
-  //     } catch (error) {
-  //       console.error("Error fetching Gates data:", error.message);
-  //     }
-  //   };
-
-  //   fetchGates();
+  //   // Fetch airports data from the server when the component mounts
+  //   fetch('http://localhost:8080/airport')
+  //     .then(response => response.json())
+  //     .then(data => setAirport(data))
+  //     .catch(error => console.error('Error fetching airports data:', error));
   // }, []);
  
-  
+  // useEffect(() => {
+  //   // Fetch airports data from the server when the component mounts
+  //   fetch('http://localhost:8080/aircraft')
+  //     .then(response => response.json())
+  //     .then(data => setAircraft(data))
+  //     .catch(error => console.error('Error fetching aircrafts data:', error));
+  // }, []);
  
-
-
-
+ 
+ 
+ 
   return (
     <div>
       <Router>
@@ -112,26 +124,24 @@ function App() {
               <Link to="/Credits" className="navbar-link">
                 Credits
               </Link>
-              <Link to="/Admin" className="navbar-link">
-                Admin
-                </Link>
+             
                  </div>
             <div className="navbar-logo">
               <img src="/src/nlflag.png" alt="Newfoundland Flag" className="logo-image" />
             </div>
           </div>
         </div>
-
+ 
         <Routes>
           <Route path="/Main" element={<Main />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Credits" element={<Credits />} />
-          <Route path="/Admin" element={<Admin />} />
-          
+         
         </Routes>
       </Router>
     </div>
   );
 }
-
+ 
 export default App;
+
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,16 +1,19 @@
-//Main page how you search
+
+ 
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-
+ 
 const Main = () => {
-  const [airport, setAirport] =useState('');
-  const [aircraft, setAircraft] =useState('');
-  const [airline, setAirline] =useState('');
-  const [gates, setGates] =useState('');
-  //These options need to be tied to a database
+  const [airport, setAirport] = useState('');
   const [airportOptions, setAirportOptions] = useState([]);
-  
+  const [aircraft, setAircraft] = useState('');
+  const [aircraftOptions, setAircraftOptions] = useState([]);
+  const [airline, setAirline] = useState('');
+  const [airlineOptions, setAirlineOptions] = useState([]);
+  const [gates, setGates] = useState('');
+  const [gatesOptions, setGatesOptions] = useState([]);
+ 
+ 
   useEffect(() => {
     const fetchAirports = async () => {
       try {
@@ -19,61 +22,145 @@ const Main = () => {
           throw new Error('Failed to fetch Airport');
         }
         const data = await response.json();
-        console.log(data);
         setAirportOptions(data._embedded?.airport || []);
       } catch (error) {
         console.error('Error fetching airport data:', error.message);
       }
     };
-  fetchAirports();
-}, []);
-// useEffect(() => {
-//   const fetchAircraft = async () => {
-//     try {
-//       const response = await fetch('http://localhost:8080/aircraft');
-//       if (!response.ok) {
-//         throw new Error('Failed to fetch Aircraft');
-//       }
-//       const data = await response.json();
-//       console.log(data);
-//       setAirportOptions(data._embedded?.aircraft || []);
-//     } catch (error) {
-//       console.error('Error fetching aircraft data:', error.message);
-//     }
-//   };
-// fetchAircraft();
-// }, []);
  
-
-
-return (
-  <div className="form-container">
-    <div className="form-box">
-      <h2>List of Airports</h2>
-      <div className="form-group">
-        <label htmlFor="airport">Select an Option:</label>
-        <select
-          id="airport"
-          value={aircraft.name || ''}
-          onChange={(e) => setAirport(e.target.value)}
-        >
-          <option value="">Select...</option>
-          {airportOptions.map((option, index) => (
-            // This changes what displayed
-            <option key={index} value={option.names}>
-              {/* This displays what is under  */}
-              {option.name}
+    fetchAirports();
+  }, []);
+ 
+  useEffect(() => {
+    const fetchAircraft = async () => {
+      try {
+        const response = await fetch('http://airportdbdocker-env.eba-r6rauie6.us-east-1.elasticbeanstalk.com/aircraft');
+        if (!response.ok) {
+          throw new Error('Failed to fetch Airport');
+        }
+        const data = await response.json();
+        setAircraftOptions(data._embedded?.aircraft || []);
+      } catch (error) {
+        console.error('Error fetching aircraft data:', error.message);
+      }
+    };
+ 
+    fetchAircraft();
+  }, []);
+ 
+  useEffect(() => {
+    const fetchAirline = async () => {
+      try {
+        const response = await fetch('http://airportdbdocker-env.eba-r6rauie6.us-east-1.elasticbeanstalk.com/airline');
+        if (!response.ok) {
+          throw new Error('Failed to fetch Airline');
+        }
+        const data = await response.json();
+        setAirlineOptions(data._embedded?.airline || []);
+      } catch (error) {
+        console.error('Error fetching airline data:', error.message);
+      }
+    };
+ 
+    fetchAirline();
+  }, []);
+ 
+  useEffect(() => {
+    const fetchGates = async () => {
+      try {
+        const response = await fetch('http://airportdbdocker-env.eba-r6rauie6.us-east-1.elasticbeanstalk.com/gates');
+        if (!response.ok) {
+          throw new Error('Failed to fetch Gates');
+        }
+        const data = await response.json();
+        setGatesOptions(data._embedded?.gates || []);
+      } catch (error) {
+        console.error('Error fetching gates data:', error.message);
+      }
+    };
+ 
+    fetchGates();
+  }, []);
+ 
+  // useEffect(() => {
+  //   const fetchAirportDetails = async () => {
+  //     if (airport) {
+  //       try {
+  //         const response = await fetch(`http://localhost:8080/airport/${airport}`);
+  //         if (!response.ok) {
+  //           throw new Error('Failed to fetch Airport details');
+  //         }
+  //         const data = await response.json();
+  //         setAirportDetails(data); // Assuming that the API response includes a property 'code'
+  //       } catch (error) {
+  //         console.error('Error fetching airport details:', error.message);
+  //       }
+  //     }
+  //   };
+ 
+  //   fetchAirportDetails();
+  // }, [airport]);
+ 
+  return (
+    <div className="form-container">
+      <div className="form-box">
+        <h2>List of Airports</h2>
+        <div className="form-group">
+          <label htmlFor="airport">Select an Airport:</label>
+          <select
+            id="airport"
+            value={airport}
+            onChange={(e) => setAirport(e.target.value)}
+            >
+             <option value="">Select...</option>
+            {airportOptions.map((option, index) => (
+            <option key={index} value={option.name}>
+            {option.name} - {option.code}
             </option>
-          ))}
-        </select>
-      </div>
-      {airport && (
-        <div>
-          <p>Name: {airport}</p>
-          <p>Aircraft: {}</p>
-          
+            ))}
+          </select>
+          <label htmlFor="airport">Select an Aircraft Tail Number:</label>
+          <select
+            id="aircraft"
+            value={aircraft}
+            onChange={(e) => setAircraft(e.target.value)}
+            >
+             <option value="">Select...</option>
+            {aircraftOptions.map((option, index) => (
+            <option key={index} value={option.name}>
+            {option.tailNumber}
+            </option>
+            ))}
+          </select>
+          <br/>
+          <label htmlFor="airport">Select an Airline: </label>
+          <select
+            id="airline"
+            value={airline}
+            onChange={(e) => setAirline(e.target.value)}
+            >
+             <option value="">Select...</option>
+            {airlineOptions.map((option, index) => (
+            <option key={index} value={option.name}>
+            {option.name}
+            </option>
+            ))}
+          </select>
+          <br/>
+          <label htmlFor="airport">Select a gate: </label>
+          <select
+            id="gates"
+            value={gates}
+            onChange={(e) => setGates(e.target.value)}
+            >
+             <option value="">Select...</option>
+            {gatesOptions.map((option, index) => (
+            <option key={index} value={option.name}>
+            {option.name}
+            </option>
+            ))}
+          </select>
         </div>
-      )}
         <div className="links">
           <Link to="/login">Sign In</Link>
         </div>
@@ -81,5 +168,5 @@ return (
     </div>
   );
 };
-
-export default Main 
+ 
+export default Main;
